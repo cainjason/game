865a3f26ccd8b0c4b31f0fcf837b82c86306b1ca
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-05-05 15:04:35 -0400

    Chore: Setup project, deleted unwanted files and bootstrapped Tailwind css

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import ReactPlayer from "react-player";
+
+function Game () {
+    return (
+        <>
+            <h1> Testing out components </h1>
+        </>
+    );
+
+}
+
+ReactDom.render(<Game />, document.getElementById('root'))
