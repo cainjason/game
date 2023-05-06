fa5c95d672d98bbce2679db8b400e446413ae4a2
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-05-06 06:17:46 -0400

    Chore: Created components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import ReactPlayer from "react-player";
+import Header from './components/Header.js';
+import Navbar from './components/Navbar.js';
+import Img from './components/Img.js';
+import Footer from './components/Footer.js';
+
 
 function Game () {
     return (
         <>
-            <h1> Testing out components </h1>
+            <Header />
+            <Navbar />
+            <Img />
             <ReactPlayer url='https://fb.watch/kl3r5xsMQB' />
+            <Footer />
         </>
     );
 
